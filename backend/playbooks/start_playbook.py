53983c08c4889fd2cec5ee11531cf3e9aeabcbe7
pieterhop <pieterhoppenbrouwers@hotmail.com>
2023-05-19 16:38:25 +0200

    Remove print statements

--- backend/playbooks/start_playbook.py
+++ backend/playbooks/start_playbook.py
@@ -9,7 +9,7 @@ def my_status_handler(data, runner_config):
     status_tracker.append(data['status'])
     print(f"Status: {data['status']}")
     if data['status'] == "failed":
-        sys.exit(f'The installation of {filename} failed. Error occured while executing: {event_tracker}.')
+        sys.exit(f'The installation of {filename} failed. Error occured while executing: {event_tracker[-1]}.')
 
 
 def my_event_handler(data):
@@ -17,14 +17,12 @@ def my_event_handler(data):
         event_data = data['event_data']
         if event_data.get('name'):
             event_tracker.append(event_data['name'])
-            print(event_data['name'])
 
 
 def main(filename):
     abs_path = pathlib.Path(__file__).parent.resolve()  # get absolute path
     parent_dir = os.path.abspath(os.path.join(abs_path, '..'))
     # Define the path to the playbook and inventory files
-    print(abs_path)
     playbook_path = f"{abs_path}/{filename}.yml"
     inventory_path = f"{private_dir}/inventory"
     # update the inventory file
